c2191aaa5e70488f0b5b08e20c0d29a8ef6867ef
elizabethqdang <elizabethqdang@gmail.com>
2020-08-03 07:52:29 -0700

    created migrations for users and notes

--- db/migrate/20201118150355_create_notes.rb
+++ db/migrate/20201118150355_create_notes.rb
@@ -1,8 +1,15 @@
 class CreateNotes < ActiveRecord::Migration[5.2]
   def change
-    create_table :notes do |t|
-
-      t.timestamps
+		create_table :notes do |t|
+			t.string	:title
+			t.string	:body
+			t.integer	:owner_id, null: false
+			t.boolean :pinned, null: false, default: false
+			t.boolean	:list, null: false, default: false
+			
+			t.timestamps
+			
+			t.index	:owner_id
     end
   end
 end
