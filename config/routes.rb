ab24cd3b75c9e898f94abe5b7b397d13c5333fb3
elizabethqdang <elizabethqdang@gmail.com>
2020-08-03 07:10:52 -0700

    configured gemfilee, database settings, and favicon

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	
+	# root to: 'static_pages#root'
+	
+	namespace :api, defaults: { format: :json } do
+		resource :session, only: [:create, :destroy]
+		resources :users, only: [:index, :create, :show, :update]
+		resources :notes, only: [:index, :create, :show, :update, :destroy]
+	end
 end
