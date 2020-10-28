cb53da88964f2d2cc621ed0647ee145ada71aaf5
elizabethqdang <elizabethqdang@gmail.com>
2020-10-28 14:20:34 -0700

    updated navbar/sidebar; updated app and root in component

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
-// import LoginForm from './sessions/login_form_container';
+import { AuthRoute, ProtectedRoute } from "../util/route_util";
+
+import LoginForm from './sessions/login_form_container';
+import SignupForm from './sessions/signup_form_container';
 import Navbar from './navbar/navbar_container';
 import Sidebar from './navbar/sidebar';
 // import NoteForm from './notes/note_form';
 import NoteIndexContainer from './notes/note_index_container';
 
 const App = () => (
-	<div className="app">
+	<div id="app" className="app">
 		<Navbar />
 		<Sidebar />
+		{/* <AuthRoute exact path="/" component={SplashContainer} /> */}
+		<AuthRoute exact path="/signup" component={SignupForm} />
+		<AuthRoute exact path="/login" component={LoginForm} />
+
 		<NoteIndexContainer />
 	</div>
 );
--- frontend/keepster.jsx
+++ frontend/keepster.jsx
@@ -19,6 +19,6 @@ document.addEventListener('DOMContentLoaded', () => {
 		store = configureStore();
 	}
 	
-	const root = document.getElementById("content");
+	const root = document.getElementById("root");
 	ReactDOM.render(<Root store={store} />, root);
 });
