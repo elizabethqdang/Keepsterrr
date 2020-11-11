73a643b175c9c5911ec13b4c571931271569e4de
elizabethqdang <elizabethqdang@gmail.com>
2020-11-11 15:11:47 -0800

    frontend note compoennts (index, note form, note edit) are pretty much completed. just need to troubleshoot event clicks

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,2 +1,23 @@
 class Note < ApplicationRecord
+	
+	validates :title, length: { maximum: 999 }
+	validates :body, length: { allow_nil: true, maximum: 9999 }
+	validates :list, inclusion: { in: [true, false] }
+	validates :pinned, inclusion: { in: [true, false] }
+
+	after_initialize { self.list = false if self.list.nil? }
+	after_initialize { self.pinned = false if self.pinned.nil? }
+	
+	belongs_to :user,
+		foreign_key: :user_id,
+		class_name: :User
+	
+	has_many :pinnned,
+		foreign_key: :pinned_id,
+		class_name: :PinnedNote
+	
+	has_many :items,
+		foreign_key: :item_id,
+		class_name: :ListItem
+		
 end
