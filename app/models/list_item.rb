7c5472d5500d75d8ebc47e3b75795969b8e89172
elizabethqdang <elizabethqdang@gmail.com>
2020-11-13 15:19:34 -0800

    WIP models

--- app/models/list_item.rb
+++ app/models/list_item.rb
@@ -1,2 +1,16 @@
 class ListItem < ApplicationRecord
+	
+	validates :item, presence: true, length = { maximum: 999 }
+	validates :completed, :inclusion: { in: [true, false] }
+	
+	after_initialize { self.completed = false if self.completed.nil? }
+
+	belongs_to :note,
+		foreign_key: :item,
+		class_name: :Note
+
+  has_one :user,
+    through: :note,
+		source: :user
+		
 end
--- app/models/pinned_note.rb
+++ app/models/pinned_note.rb
@@ -1,2 +1,6 @@
+
 class PinnedNote < ApplicationRecord
+	validates :note_id, uniqueness: true
+	
+	belongs_to :note
 end
